4d5711af8dc204cc9c7ac70f2eea040c0da91b7f
Jayant gupta <jayantkumargupta41l@example.com>
2025-05-01 19:43:55 +0530

    updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -663,7 +663,6 @@ const Portfolio = () => {
                                                 whileHover={{ scale: 1.1 }}
                                             />
                                             <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-6">
-
                                             </div>
                                         </div>
 
